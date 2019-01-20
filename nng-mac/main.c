89aef1dae2fedaa08e9efcb6c7564d34bf551f43
Samuel Wu <samuel.wu@btnussfom0099.local>
2019-01-20 10:14:21 -0800

    Initial Commit

---
+++ nng-mac/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  nng-mac
+//
+//  Created by Samuel Wu on 1/20/19.
+//  Copyright © 2019 Samuel Wu. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
